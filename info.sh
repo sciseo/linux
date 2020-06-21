a50bcc490a879113bf980f08912f82c401691f1a
Dimo <dimobelov@outlook.com>
2020-06-21 12:50:03 +0300

    Update info.sh

--- info.sh
+++ info.sh
@@ -16,7 +16,7 @@ echo -e "System Main IP:\t\t"`hostname -I`
 echo ""
 echo -e "-------------------------------CPU/Memory Usage------------------------------"
 echo -e "Memory Usage:\t"`free | awk '/Mem/{printf("%.2f%"), $3/($2+.000001)*100}'`
- echo -e "Swap Usage:\t"`free | awk '/Swap/{printf("%.2f%"), $3/($2+.000001)*100}'`
+echo -e "Swap Usage:\t"`free | awk '/Swap/{printf("%.2f%"), $3/($2+.000001)*100}'`
 echo -e "CPU Usage:\t"`cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
 echo ""
 echo -e "-------------------------------Disk Usage >80%-------------------------------"
