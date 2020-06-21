8d7003e779bd5935031bdf38d3994f0cf8def45c
Dimo <dimobelov@outlook.com>
2020-06-21 12:50:58 +0300

    Update info.sh

--- info.sh
+++ info.sh
@@ -19,5 +19,5 @@ echo -e "Memory Usage:\t"`free | awk '/Mem/{printf("%.2f%"), $3/($2+.000001)*100
 echo -e "Swap Usage:\t"`free | awk '/Swap/{printf("%.2f%"), $3/($2+.000001)*100}'`
 echo -e "CPU Usage:\t"`cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
 echo ""
-echo -e "-------------------------------Disk Usage >80%-------------------------------"
+echo -e "-------------------------------Disk Usage >10%-------------------------------"
 df -Ph | sed s/%//g | awk '{ if($5 > 10) print $0;}'
